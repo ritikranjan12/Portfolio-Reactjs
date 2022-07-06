a6c8e33b46e47ce80dff27bb215510f97ceda488
Ritik <ritik123453@gmail.com>
2022-07-07 00:09:35 +0530

    Changing sanity config files

--- src/client.js
+++ src/client.js
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = sanityClient({
-    projectId : "fvau70ts",
+    projectId : process.env.REACT_APP_SANITY_PROJECT_ID,
     dataset:'production',
     apiVersion:'2022-02-01',
-    useCdn:true,
-    token: "skTGZtyIpp8GDASABRmTHzmyhrgvYJg77AL8YQIiXtM39qGKDwmojfZSg3mJ2GRmYeta7bouPV00dqvlF2oMNdITrmO5YyRsUVPtuC7rMnQUZ54lhYLXfM9ZOrUIT4anDY4CO0PGQjLzEwbgfE3WB5qUNgN0JQba3ZwpW6RSgO4B7xMwUKRE",
+    useCdn: true,
+    token: process.env.REACT_APP_SANITY_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
